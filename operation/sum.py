b001e2bc207af6a64ddd03a4978bccaf2b0b9d72
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2026-08-12 19:51:33 +0700

    init project
    fix: fix ci
    
    add dependabot

---
+++ operation/sum.py
@@ -0,0 +1,12 @@
+def sum(a: int, b: int) -> int:
+    """
+    Returns the sum of two integers.
+
+    Parameters:
+    a (int): The first integer.
+    b (int): The second integer.
+
+    Returns:
+    int: The sum of a and b.
+    """
+    return 0
